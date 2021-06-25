16c309ac013a9d5283e16db5bb6d258e528f907f
tong <tong@disktree.net>
2021-06-25 14:36:03 +0200

    …

--- blender/enable_addon.py
+++ blender/enable_addon.py
@@ -2,7 +2,7 @@ import os
 import bpy
 from addon_utils import check,paths,enable
 
-if(!check("armory")[1]):
+if not check("armory")[1]:
     bpy.ops.preferences.addon_install(filepath='armsdk/armory.py')
     bpy.ops.preferences.addon_enable(module='armory')
     bpy.context.preferences.addons["armory"].preferences['sdk_path'] = os.getcwd()+'/armsdk/'
